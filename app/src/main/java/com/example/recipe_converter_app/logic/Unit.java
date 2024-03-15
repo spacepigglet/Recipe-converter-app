fc4debaa6afece288c3b2267960eba4a9ced82a2
spacepigglet <97839498+spacepigglet@users.noreply.github.com>
2024-03-15 01:16:33 +0100

    Unit enum

---
+++ app/src/main/java/com/example/recipe_converter_app/logic/Unit.java
@@ -0,0 +1,22 @@
+package com.example.recipe_converter_app.logic;
+
+public enum Unit {
+    GRAM("g"),
+    KILOGRAM("kg"),
+    MILLILITER("mL"),
+    DECILITER("dL"),
+    LITER("L"),
+    TEASPOON("tsk"),
+    TABLESPOON("msk"),
+    SPICE("krm"),
+    PIECE("st");
+
+    private final String name;
+    Unit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
