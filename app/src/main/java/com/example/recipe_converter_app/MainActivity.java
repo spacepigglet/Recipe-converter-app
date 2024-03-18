3e567312e25d8bcff3a3b99f88b3b10f852c337f
spacepigglet <97839498+spacepigglet@users.noreply.github.com>
2024-03-18 21:51:55 +0100

    Back button gone on all top level fragments (in bottom nav bar)

--- app/src/main/java/com/example/recipe_converter_app/MainActivity.java
+++ app/src/main/java/com/example/recipe_converter_app/MainActivity.java
@@ -31,10 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 //R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 //.build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-
+        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home,
+                R.id.navigation_my_recipes,
+                R.id.navigation_notifications
+        ).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
     @Override
     public boolean onSupportNavigateUp() {
